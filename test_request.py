e348f9e9e5787e81db1caf6eb46e3697d9092952
Kostya Gonciarou <go.toba@gmail.com>
2023-11-18 15:47:54 +0400

    ip generator

--- test_request.py
+++ test_request.py
@@ -1,4 +1,6 @@
+from random import randint
 from unittest import mock
+import pytest
 
 import requests
 from requests import Response
@@ -6,13 +8,31 @@ from requests import Response
 
 def get_my_ip():
     response = requests.get(
-        'http://ipinfo.io/json'
+        'https://ipinfo.io/json'
     )
     return response.json().get('ip')
 
 
-def test_get_my_ip(monkeypatch):
-    my_ip = '123.123.123.123'
+@pytest.fixture
+def ipv4address():
+    """
+    Generates a random ipv4 address.
+    """
+    class IPv4AddrFactory:
+        """Generates random ipv4 addresses."""
+        @staticmethod
+        def get():
+            """Return an ipv4 address."""
+            ipaddr = f"{randint(1, 255)}."
+            f"{randint(1, 255)}."
+            f"{randint(1, 255)}."
+            f"{randint(1, 255)}"
+            return ipaddr
+    return IPv4AddrFactory()
+
+
+def test_get_my_ip(monkeypatch, ipv4address):
+    my_ip = ipv4address
     response = mock.create_autospec(Response)
     response.json.return_value = {'ip': my_ip}
 
@@ -23,4 +43,3 @@ def test_get_my_ip(monkeypatch):
     )
 
     assert get_my_ip() == my_ip
-
